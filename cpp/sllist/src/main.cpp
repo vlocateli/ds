d1a861cb6787fd60be551630179000b1c1ff7b81
victor <victorlocateli999@gmail.com>
2024-10-03 01:07:07 -0300

    fixed seg fault

--- cpp/sllist/src/main.cpp
+++ cpp/sllist/src/main.cpp
@@ -1,13 +1,11 @@
 #include "../include/sllist.hpp"
-#include "../include/timer.hpp"
 int main () {
     ds::LinkedList<size_t> list_1;
 
     for (size_t i = 1; i < 10; i++) {
         list_1.push_back(i);
     }
-    ds::LinkedList<size_t> list_2(list_1);
-    std::cout << list_2 << '\n';
+    std::cout << list_1 << '\n';
     return 0;
 }
 
