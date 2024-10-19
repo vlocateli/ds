1851fa3e384f57b3860bbce8ab077d7757b0b719
victor <victorlocateli999@gmail.com>
2024-10-19 19:11:56 -0300

    Added implementation to some Single Linked List functions

--- cpp/sllist/include/sllist.hpp
+++ cpp/sllist/include/sllist.hpp
@@ -1,8 +1,20 @@
+#ifndef SLLIST_HPP
+#define SLLIST_HPP 1
 #pragma once
-#include "node.hpp"
 #include <iostream>
 #include <assert.h>
-#include <limits>
+#include "node.hpp"
+#include "sllistiterator.hpp"
+#define CHECK_BOUNDARIES 1 
+#define DEBUG 1
+#ifdef DEBUG
+#include <iostream>
+#ifndef CHECK_BOUNDARIES
+#define CHECK_BOUNDARIES 1 
+#endif // CHECK_BOUNDARIES
+#define DEBUG_PRINT(x)\
+    std::cerr << #x << ": " << x << '\n';
+#endif // DEBUG
 namespace ds{
     template<typename T>
     class LinkedList{
@@ -49,6 +61,17 @@ namespace ds{
             }
             LinkedList& operator= (const LinkedList& list)
             {
+                if (this == &list) {
+                    return *this;
+                }
+                clear();
+                auto tmp = list.m_list;
+                while (tmp != nullptr) {
+                    push_back(list.value_ref());
+                    tmp = tmp->next();
+                }
+
+                return *this;
             }
             LinkedList& operator= (LinkedList&& other)
             {
@@ -77,6 +100,36 @@ namespace ds{
                     tmp = tmp->next();
                     other = other->next();
                 }
+            }
+            bool is_empty() const
+            {
+                return m_size == 0;
+            }
+            void clear()
+            {
+                deallocate_list();
+                m_list = nullptr;
+                m_tail = nullptr;
+                m_size = 0;
+            }
+            T& at(const size_t index) const
+            {
+                auto tmp = m_list;
+#ifdef CHECK_BOUNDARIES
+                if (index >= m_size) {
+                    DEBUG_PRINT(index);
+                    throw std::out_of_range
+                          ("ERROR: Index out of range in at()\n");
+                }
+#endif // CHECK_BOUNDARIES
+                if (index == m_size - 1) {
+                    return m_tail->value_ref();
+                }
+                for (size_t i = 0; i < index; i++) {
+                    tmp = tmp->next();
+                }
+                return tmp->value_ref();
+
             }
             ~LinkedList() noexcept
             {
@@ -114,15 +167,25 @@ namespace ds{
             }
             T pop_back()
             {
-                auto tmp = m_list;
-                ds::SLLNode<T> *before = nullptr;
-                while (tmp->next() != nullptr) {
-                    before = tmp;
-                    tmp = tmp->next();
+                auto head = m_list;
+                ds::SLLNode<T>* previous = nullptr;
+                T pop_value {};
+                if (m_size == 1) {
+                    pop_value = head->value();
+                    deallocate_list();
+                    return pop_value;
                 }
-               
-                std::cout << tmp->value() << '\n';
-                return (0);
+                while (head != m_tail) {
+                    previous = head;
+                    head = head->next();
+                }
+                previous->set_next(nullptr);
+                auto tmp = head;
+                pop_value = head->value();
+                delete tmp;
+                m_tail = previous;
+                m_size--;
+                return pop_value;
             }
             const T* search(const T& val)
             {
@@ -135,9 +198,10 @@ namespace ds{
                 }
                 return nullptr;
             }
-            T minimum(){
+            T minimum() const
+            {
                 auto tmp = m_list;
-                T minimum = std::numeric_limits<T>::min(); 
+                T minimum = tmp->value(); 
                 while (tmp != nullptr) {
                    if (tmp->value() > minimum) {
                        minimum = tmp->value();
@@ -146,10 +210,10 @@ namespace ds{
                 }
                 return minimum;
             }
-            T maximum() 
+            T maximum() const 
             {
                 auto tmp = m_list;
-                T maximum = std::numeric_limits<T>::max();
+                T maximum = tmp->value(); 
                 while (tmp != nullptr) {
                     if (tmp->value() < maximum) {
                         maximum = tmp->value();
@@ -157,6 +221,70 @@ namespace ds{
                     tmp = tmp->next();
                 }
                 return maximum;
+            }
+            T find_middle() const
+            {
+                size_t mid_index = m_size;
+                mid_index  = mid_index % 2 == 0 ? mid_index/2:(mid_index/2) + 1; 
+                T middle_value = at(mid_index);
+                return middle_value; 
+                
+            }
+            size_t find(const T& value) const
+            {
+                if (m_tail->value() == value) {
+                    return m_size - 1;
+                }
+                size_t i;
+                for (i = 0; i < m_size - 1; i++) {
+                   if (this->at(i) == value) {
+                       break;
+                   }
+                }
+                return i;
+            }
+            T pop_front()
+            {
+                auto head = m_list;
+                ds::SLLNode<T>* previous = nullptr;
+                T pop_value {};
+                if (m_size == 1) {
+                    pop_value = m_list->value();
+                    deallocate_list();
+                    return pop_value;
+                }
+                previous = head;
+                head = head->next();
+                m_list = head;
+                pop_value = previous->value();
+                delete previous;
+                m_size--;
+                return pop_value;
+            }
+            bool remove(const T& value)
+            {
+                size_t index = this->find(value);
+                if (index == 0) {
+                    pop_front();
+                    m_size--;
+                    return true;
+                }
+                else if (index == m_size - 1) {
+                    pop_back();
+                    m_size--;
+                    return true;
+                }
+                ds::SLLNode<T> *previous = nullptr;
+                auto head = m_list->next();
+                for (size_t i = 1; i < index; i++) {
+                    previous = head;
+                    head = head->next();
+                }
+                previous->set_next(head->next());
+                auto node_to_delete = head;
+                delete node_to_delete;
+                m_size--;
+                return true;
             }
             friend std::ostream& operator<< (std::ostream& stream,
                                       const ds::LinkedList<T> &list)
@@ -170,6 +298,14 @@ namespace ds{
 
                 return stream;
             }
+            ds::detail::Iterator<T> begin()
+            {
+            return ds::detail::Iterator<T>(m_list);
+            }
+            ds::detail::Iterator<T> end()
+            {
+                return ds::detail::Iterator<T>(nullptr);
+            }
         private:
             void deallocate_list() noexcept
             {
@@ -180,10 +316,15 @@ namespace ds{
                     head = head->next();
                     delete p;
                 }
+                m_list = nullptr;
+                m_tail = nullptr;
+                m_size = 0;
                 
             }
             SLLNode<T>* m_list;
             SLLNode<T>* m_tail;
             size_t   m_size;
-    };
-}
+
+};
+} // namespace ds
+#endif // SLLIST_HPP
